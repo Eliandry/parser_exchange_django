9653a0d71effc174a7ed188d5da1cdc88a8e7de9
andrey <example@gmail.com>
2020-03-31 18:45:34 +0300

    улучшена скорость работы.Курсы сохраняются в БД

--- df/models.py
+++ df/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Person(models.Model):
     email=models.EmailField()
     time=models.TimeField()
+class Corren(models.Model):
+    doll = models.FloatField()
+class Corren_euro(models.Model):
+    euro = models.FloatField()
--- df/views.py
+++ df/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Person
+from .models import Person, Corren , Corren_euro
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -13,13 +13,6 @@ class Currency:
     DOLLAR = 'https://www.google.ru/search?newwindow=1&client=opera&hs=7Mo&sxsrf=ALeKk009g0zJ8nOlsPbtQCZb4XwvdSa2oQ%3A1585300321696&ei=YcN9XtCRKsHh6QTYwraYBQ&q=доллар+к+рублю&oq=доллар+к+рублю&gs_l=psy-ab.3..0i131i70i258j0j0i131l2j0l6.2325305.2342192..2342552...14.1..0.226.3774.0j24j1......0....1..gws-wiz.....10..0i71j35i362i39j0i10i1j0i10i1i42j0i10j0i13i30.jG-yUAz0h7s&ved=0ahUKEwiQxqX0p7roAhXBcJoKHVihDVMQ4dUDCAo&uact=5'
     user = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 OPR/67.0.3575.97'}
-    converted_price_doll = 0
-    converted_price_euro = 0
-    diff = 3
-
-    def __init__(self):
-        self.converted_price_doll = float(self.get_price_doll().replace(",", "."))
-        self.converted_price_euro = float(self.get_price_euro().replace(",", "."))
 
     def get_price_doll(self):
         fullPage_dollar = requests.get(self.DOLLAR, headers=self.user)
@@ -55,12 +48,26 @@ class Currency:
 
 
 currency = Currency()
-
+def create_currency_in_bd(dol=currency.check_doll(),euro=currency.check_euro()):
+    global time
+    Corren.objects.create(doll=dol)
+    Corren_euro.objects.create(euro=euro)
 
 def index(request):
-    ans_doll = currency.check_doll()
-    ans_euro = currency.check_euro()
+    h=None
+    f=Corren.objects.all()
+    h=[i for i in f]
+
+    create_currency_in_bd()
     people = Person.objects.all()
+    try:
+        a=Corren.objects.get(id=len(h)-1)
+        b=Corren_euro.objects.get(id=len(h)-1)
+    except:
+        create_currency_in_bd()
+        return HttpResponseRedirect('/')
+    ans_doll = a.doll
+    ans_euro = b.euro
     return render(request, "index.html", {"currency_doll": ans_doll, "currency_euro": ans_euro, "people": people})
 
 
